e7ed613fd8b70d6b1e9789dcd89091c40b7ea1c6
Maria Alavez <mariaalavez@hotmail.com>
2015-11-30 09:43:25 +0100

    Start splitting into Board and Game class

--- board.rb
+++ board.rb
@@ -2,6 +2,8 @@ require 'byebug'
 
 class Board
 
+  attr_accessor :game
+
   def initialize
     @board = {
       1 => '.', 2 => '.', 3 => '.',
@@ -10,7 +12,31 @@ class Board
     }
   end
 
-  def draw_board
-    
+  def set_mark_at_position(position, mark)
+    #Places a mark on the board at a given position
+  end
+
+  def look_up_for_mark_at_position(position)
+    # Returns the mark at given position X, O or nil
+  end
+
+  def display_board
+    # Prints the board to the terminal with no format
+  end
+
+  def valid_position?(position)
+    # Returns true or false if position is an integer
+
+    allowed_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    if allowed_positions.include? position
+      @chosen_position = position
+    else
+      puts "This is not a valid position"
+    end
+  end
+
+  def valid_move?(position)
+    # REturns true or false if position is taken
   end
 end
--- game.rb
+++ game.rb
@@ -1,18 +1,33 @@
 require 'byebug'
-require 'board.b'
+require './board'
 
 class Game
 
+  attr_accessor :board
+
   def initialize
+    @board = Board.new
     @round = 1
     @game_over = false
-    @board = Board.new
   end
 
-  def self.play
-    puts @board
+  def draw_board
+    # Prints our board nicely
+  end
+
+  def ask_users_position
+    puts "\n Choose a position between 1 and 9: "
+    position = gets.chomp.to_i
+  end
+
+  def play
+    ask_users_position
+    # Ask user position
+    # Updates board with users position
+    # Print board
   end
 end
 
 
-g = Game.play
+g = Game.new
+g.play
